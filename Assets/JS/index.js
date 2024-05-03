da0c04f7341cda8459191c9dc52e70ed0b285a2d
primewrld <118029296+primewrld@users.noreply.github.com>
2024-05-03 11:09:01 +0100

    first commit

---
+++ Assets/JS/index.js
@@ -0,0 +1,16 @@
+function openBar() {
+    document.getElementById("btbar").style.width = "350px";
+  }
+  
+  function closeBar() {
+    document.getElementById("btbar").style.width = "0";
+  }
+
+
+function openNav() {
+    document.getElementById("firstSidebar").style.width = "350px";
+  }
+  
+  function closeNav() {
+    document.getElementById("firstSidebar").style.width = "0";
+  }
